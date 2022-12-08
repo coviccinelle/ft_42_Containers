8e190cfce9ea66b381daba7671477ee3ca142818
coviccinelle_mac <thi-phng@student.42.fr>
2022-12-08 14:35:56 +0100

    WIP

---
+++ cpp_project_sample/include/ft/vector.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/08 10:59:18 by thi-phng          #+#    #+#             */
+/*   Updated: 2022/12/08 11:54:36 by thi-phng         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+
+#include <iostream>
+
+// A template is a simple yet very powerful tool in C++. The simple idea is to pass
+// data type as a parameter so that we don’t need to write the same code for different
+// data types. For example, a software company may need to sort() for different data types. 
+// Rather than writing and maintaining multiple codes, we can write one sort() and pass 
+// data type as a parameter. 
+// C++ adds two new keywords to support templates: ‘template’ and ‘typename’.
+// The second keyword can always be replaced by the keyword ‘class’.
+
+// Class template
+namespace ft {
+
+	template<typename Type>
+	class vector {
+		public:
+		private:
+	};
+} // end of namespace
+#endif
