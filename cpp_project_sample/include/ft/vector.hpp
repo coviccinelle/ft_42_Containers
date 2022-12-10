afc8be7f41b6d38ac087a8314f07936087c1665f
coviccinelle_mac <thi-phng@student.42.fr>
2022-12-10 14:51:30 +0100

    before changing to gtest 1.12 -> 1.8.1

--- cpp_project_sample/include/ft/vector.hpp
+++ cpp_project_sample/include/ft/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 10:59:18 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/12/10 11:02:05 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/12/10 13:36:22 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,30 +30,56 @@ namespace ft {
 	template<typename Type>
 	class vector {
 		public:
-			vector(void) : _c_size(0){
-				this->_c_data = this->alloc.allocate(4);
-				std::memset(this->_c_data, 0, 4);
+			vector(void) : _c_size(0), _capacity(2) {
+				this->_c_data = this->alloc.allocate(this->_capacity + 1);//void deallocate( T* p, std::size_t n ); //pointer allocate( size_type n, const void * hint = 0 );
+				std::memset(this->_c_data, 0, this->_capacity + 1);
 			};
-
 			~vector(void){};
 
-			using size_type = std::size_t;
-			size_type size() const{
-				return (_c_size);
-			}
+			typedef std::size_t size_type;//typedef = using
+
+			size_type size() const { return (_c_size); }
 
 			void push_back( const Type& value ){
-				//size_t i = 0;
-				(void)value;
-				//Type *new_data = this->alloc.allocate(_c_size + 20);
-				//if (this->_c_size == 0)
-				//	_c_data[0] = value;
-				//for (size_t i2 = 0; i2 <= _c_size; i2++){
-					//new_data[i] = _c_data[i];}
-			//	_c_data[i] = value;
+				size_t old_capacity = this->_capacity;
+				for (size_t i = 0; i < this->_c_size; i++)
+					std::cout << this->_c_data[i] << " : " << this->_c_size << std::endl;
+				std::cout << std::endl;
+				if (this->_c_size == this->_capacity){
+					this->_capacity *= 2;
+					Type *new_data = this->alloc.allocate(this->_capacity + 1);
+					std::memset(this->_c_data, 0, this->_capacity + 1);
+					size_t i = 0;
+					for (; i < _c_size; i++){
+						new_data[i] = _c_data[i];
+					}
+					new_data[i] = value;
+					alloc.deallocate(_c_data, old_capacity + 1);
+					_c_data = new_data;
+					//this->_capacity = new_capacity;
+				}
+				else
+				{
+					_c_data[this->_c_size] = value;
+				}
 				this->_c_size++;
 			}
-
+//			 Modifiers
+  			//void push_back(const T& value) {
+  			//  if (m_size >= m_capacity) {
+  			//    // Need to allocate more memory
+  			//    size_t new_capacity = (m_capacity > 0) ? m_capacity * 2 : 1;
+  			//    T* new_data = new T[new_capacity];
+  			//    for (size_t i = 0; i < m_size; i++) {
+  			//      new_data[i] = m_data[i];
+  			//    }
+  			//    delete[] m_data;
+  			//    m_data = new_data;
+  			//    m_capacity = new_capacity;
+  			//  }
+  			//  m_data[m_size] = value;
+  			//  m_size++;
+  			//}
 			void pop_back()
 			{
 				if (_c_size == 0)
@@ -76,10 +102,7 @@ namespace ft {
 			size_t	_c_size;
 			Type *	_c_data;
         	std::allocator< Type > alloc;
-			//size_t	_max_place;//capacity
-
+			size_t	_capacity;
 	};
 } // end of namespace
-
-
 #endif
--- cpp_project_sample/src/main.cpp
+++ cpp_project_sample/src/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
-
+#include <ft/vector.hpp>
 int	main(void)
 {
 	std::cout << "Hello World !" << std::endl;
+	ft::vector<int> NewVector;
+	NewVector.push_back(42);
+	std::cout << "Play 42 please :" << NewVector[0] << std::endl;
 	return (0);
 }
 
